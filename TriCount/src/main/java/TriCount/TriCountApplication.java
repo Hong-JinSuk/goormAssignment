817b89406d3afc144b68503afee7075267d3cad1
RealTone <realtone98@gmail.com>
2024-05-12 15:33:19 +0900

    tricount 클론코딩 시작

---
+++ TriCount/src/main/java/TriCount/TriCountApplication.java
@@ -0,0 +1,13 @@
+package TriCount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriCountApplication.class, args);
+	}
+
+}
---
+++ TriCount/src/test/java/TriCount/TriCountApplicationTests.java
@@ -0,0 +1,13 @@
+package TriCount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
